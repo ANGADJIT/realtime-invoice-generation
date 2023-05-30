49b0638bb1a9b8bc956d9a6ffdac02c29f39d249
Angadjit singh <angad38.agcs@gmail.com>
2023-05-30 23:10:54 +0530

    added lambda job

--- invoice_lambda_job/src/invoice_generator.py
+++ invoice_lambda_job/src/invoice_generator.py
@@ -3,6 +3,7 @@ import boto3
 from os import environ
 import base64
 import json
+from render_pdf import RenderPdf
 
 
 def generate_invoice(event, context) -> Any:
@@ -10,18 +11,24 @@ def generate_invoice(event, context) -> Any:
     s3 = boto3.client('s3', region_name=environ.get('REGION_NAME'),
                       endpoint_url=environ.get('ENDPOINT_URL'))
 
-    for index, record in enumerate(event['Records']):
-        try:
-            # data = base64.b64decode(
-            #     record['kinesis']['data'])decode('latin-1')
-            data = type(record['kinesis']['data'])
+    if 'Records' in event:
+        # get record and convert into dict
+        record: str = event['Records'][0]['kinesis']['data']
 
-            s3.put_object(Body=str(data),
-                          Bucket='invoices', Key=f'type{index}.txt')
-            s3.put_object(Body=str(record['kinesis']['data']),
-                          Bucket='invoices', Key=f'data{index}.txt')
-        except (Exception) as e:
-            s3.put_object(Body=str(e),
-                          Bucket='invoices', Key=f'error{index}.txt')
+        decoded_record: bytes = base64.b64decode(record)
+        json_string: str = decoded_record.decode('utf-8')
+
+        data: dict = json.loads(json_string)
+
+        transaction_id: str = data['transaction_id']
+
+        # make invoice from data
+        render_pdf: RenderPdf = RenderPdf(data=data)
+        pdf: bytes = render_pdf()
+
+        s3.put_object(Body=pdf,
+                      Bucket=environ.get('BUCKET'), Key=f'{transaction_id}.pdf')
+
+        return {'message': 'data written in s3'}
 
     return {'error': 'data not found'}
--- invoice_lambda_job/src/render_pdf.py
+++ invoice_lambda_job/src/render_pdf.py
@@ -1,6 +1,8 @@
 from jinja2 import Template
-import xhtml2pdf.pisa as pisa
-from io import BytesIO
+import convertapi
+from os import environ
+from os.path import exists
+from os import remove
 
 
 class RenderPdf:
@@ -14,15 +16,34 @@ class RenderPdf:
 
             self.__rendered_invoice = invoice_template.render(**self.__data)
 
-    def __render_pdf(self):
-        invoice_pdf = BytesIO()
-        pisa.CreatePDF(self.__rendered_invoice, dest=invoice_pdf)
+    def __render_pdf(self) -> bytes:
+        rendered_html_file: str = '/tmp/rendered_invoice.html'
+        rendered_html_pdf: str = '/tmp/rendered_invoice.pdf'
 
-        return invoice_pdf.getvalue()
+        # save file before covertion
+        with open(rendered_html_file, 'w') as rendered:
+            rendered.write(self.__rendered_invoice)
+
+        # add secret to api
+        convertapi.api_secret = environ.get('SECRET')
+
+        convertapi.convert('pdf', {
+            'File': rendered_html_file
+        }, from_format='html').save_files(rendered_html_pdf)
+
+        # re-open and covert back to bytes format
+        with open(rendered_html_pdf, 'rb') as bytes_file:
+            pdf_file: bytes = bytes_file.read()
+
+        # delete temp file
+        if exists(rendered_html_file) and exists(rendered_html_pdf):
+            remove(rendered_html_file)
+            remove(rendered_html_pdf)
+
+        return pdf_file
 
     def __call__(self) -> bytes:
         self.__render_html()
         invoice_pdf: bytes = self.__render_pdf()
 
         return invoice_pdf
-
